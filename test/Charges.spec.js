3f97f048a31945153221f5c91fbaad291011a187
Khom Sovon <khom.sovon@gmail.com>
2020-03-20 10:08:20 +0700

    Charge spec

--- test/Charges.spec.js
+++ test/Charges.spec.js
@@ -1,14 +1,14 @@
-const assert = require('chai').assert;
+const expect = require('chai').expect;
 const bongloy = require('./Bongloy');
 
-describe('Charge', function(){
+describe('Charges', function(){
   function getToken(){
     return new Promise(function(resolve, reject) {
       bongloy.tokens.create({
           card: {
             number: '6200000000000005',
             exp_month: 3,
-            exp_year: 2021,
+            exp_year: new Date().getFullYear() + 1,
             cvc: '123',
           },
         },function(err, token) {
@@ -27,9 +27,9 @@ describe('Charge', function(){
         source: token.id
       },
       function(err, charge) {
-        assert.equal(charge.amount, 10000);
-        assert.equal(charge.currency, "USD");
-        assert.equal(charge.status, "succeeded");
+        expect(charge.amount).to.equal(10000);
+        expect(charge.currency).to.equal("USD");
+        expect(charge.status).to.equal("succeeded");
       });
   });
 
@@ -44,7 +44,7 @@ describe('Charge', function(){
       },
       function(err, charge) {
         bongloy.charges.retrieve(charge.id, function(err, result){
-          assert.equal(result.id, charge.id);
+          expect(result.id).to.equal( charge.id);
         })
       });
   });
@@ -59,8 +59,8 @@ describe('Charge', function(){
         source: token.id
       },
       function(err, charge) {
-        bongloy.charges.list({limit: 3}, function(err, charges){
-          assert.equal(charges.data[0].id, charge.id);
+        bongloy.charges.list(function(err, charges){
+          expect(charges.data.map((ch) => ch.id)).to.include.members([charge.id]);
         })
       });
   });
@@ -77,9 +77,9 @@ describe('Charge', function(){
       },
       function(err, charge) {
         bongloy.charges.capture(charge.id, function(error, result){
-          assert.equal(result.captured, true);
-          assert.equal(result.status, "succeeded");
-          assert.equal(result.amount, 10000);
+          expect(result.captured).to.equal(true);
+          expect(result.status).to.equal("succeeded");
+          expect(result.amount).to.equal(10000);
         });
       });
   });
@@ -96,8 +96,8 @@ describe('Charge', function(){
       },
       function(err, charge) {
         bongloy.charges.capture(charge.id, {amount: 8000}, function(error, result){
-          assert.equal(result.captured, true);
-          assert.equal(result.amount_refunded, 2000);
+          expect(result.captured).to.equal(true);
+          expect(result.amount_refunded).to.equal(2000);
         });
       });
   });
